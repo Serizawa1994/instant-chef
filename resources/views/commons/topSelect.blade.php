d06df7db32b6656c484c36b8fe8f87ed12748d3a
Serry <riptonlemon@yahoo.co.jp>
2019-08-17 09:28:32 +0000

    delete navbar

--- resources/views/commons/topSelect.blade.php
+++ resources/views/commons/topSelect.blade.php
@@ -3,33 +3,6 @@
         padding: 0;
     }
 </style>
-<nav class="navbar navbar-expand-sm navbar-light bg-light">
-            <!-- ホームへ戻るリンク。ブランドロゴなどを置く。 -->
-            <a href="#" class="navbar-brand">P&amp;K</a>
-            
-            <!-- 横幅が狭い時に出るハンバーガーボタン -->
-            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            
-            <!-- メニュー項目 -->
-            <div class="collapse navbar-collapse" id="nav-bar">
-                <ul class="navbar-nav">
-                    <li class="nav-item active"><a href="#" class="nav-link">子犬</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">子猫</a></li>
-                    <li class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">会社情報</a>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#">ビジョン</a>
-                            <a class="dropdown-item" href="#">会社概要</a>
-                            <a class="dropdown-item" href="#">地図</a>
-                            <a class="dropdown-item" href="#">沿革</a>
-                        </div>
-                    </li>
-                    <li class="nav-item"><a href="#" class="nav-link">お問い合わせ</a></li>
-                </ul>
-            </div>
-        </nav>
 
 <aside style="margin-top:30px;">
     <nav class="navbar navbar-expand-sm navbar-light">
